6d78478a0e12c57aed8839ebee4297b095528052
angelajarae <angelajaraespinoza@gmail.com>
2024-06-30 21:33:32 -0500

    Precess videos

--- escaner.py
+++ escaner.py
@@ -15,20 +15,20 @@ def a_binario(frame):
     kernel = np.ones((2, 2), np.uint8)
     binary = cv2.erode(binary, kernel, iterations=1)
     binary = cv2.dilate(binary, kernel, iterations=1)
-    cv2.imshow("Imagen binaria tratada",binary)
-    cv2.waitKey(0) 
+    #cv2.imshow("Imagen binaria tratada",binary)
+    #cv2.waitKey(0) 
     return binary
 
 def hallar_contornos(binary):
     # Canny edge detection
     canny = cv2.Canny(binary, 100, 200)
-    cv2.imshow("Bordes",canny)
-    cv2.waitKey(0) 
+    #cv2.imshow("Bordes",canny)
+    #cv2.waitKey(0) 
     # Find contours
     contours, hierarchy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     return contours
 
-def media_puntos(contornos):
+def media_puntos(frame,contornos):
     mPuntos = {}
     if len(contornos) > 0:
         for contour in contornos:
@@ -39,63 +39,58 @@ def media_puntos(contornos):
                 else:
                     mPuntos[y] = x
     mPuntos = [(x, y) for y, x in mPuntos.items()]
+    # height, width = frame.shape[:2]
+    # visualization = np.zeros((height, width, 3), dtype=np.uint8)
+    # for x,y in mPuntos:
+    #     cv2.circle(visualization, (int(x), int(y)), 1, (0, 255, 0), -1)
+    # cv2.imshow("Puntos 2D",visualization)
+    # cv2.waitKey(0) 
     return mPuntos
 
 def hallar_puntos_3d(puntos):
     global angulo_laser_y_camara,angulo_recorrido
     angle_laser_camara_radians = np.deg2rad(angulo_laser_y_camara)
     angle_recorrido_radians = np.deg2rad(angulo_recorrido)
-    with open('3d_coordinates.txt', 'w') as file:
+    with open('3d_coordinates.txt', 'a') as file:
         for (x, y) in puntos:
             r = x / np.sin(angle_laser_camara_radians)
             z = np.sqrt(r**2 - x**2)
             x_3d = x * np.cos(angle_recorrido_radians) - z * np.sin(angle_recorrido_radians)
             z_3d = x * np.sin(angle_recorrido_radians) + z * np.cos(angle_recorrido_radians)
-            
             # Write coordinates to file
             file.write(f'{x_3d},{y},{z_3d}\n')
-    
-    print("3D Coordinates written to 'coordinates.txt'")
+
 
 
 
 def main():
-        global angulo_recorrido
-  # USING A VIDEO
-  # cap = cv2.VideoCapture('video.mp4')
-  # frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-  # angle_per_frame = 360.0 / frame_count
+    global angulo_recorrido
 
-  # if not cap.isOpened():
-  #     print("Error: Could not open video.")
-  #     return
+    #USING A VIDEO
+    cap = cv2.VideoCapture('video.mp4')
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    angle_per_frame = 360.0 / frame_count
 
-  # while cap.isOpened():
-  #     ret, frame = cap.read()
-  #     if not ret:
-  #         break  # End of video
+    if not cap.isOpened():
+        print("Error: Could not open video.")
+        return
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break  # End of video
 
         #USING AN IMAGE
-        frame = cv2.imread("imagen4.png")
+        #frame = cv2.imread("imagen1.png")
 
         binary=a_binario(frame)
-        
-        contours=hallar_contornos(binary)
-        
-        mPuntos = media_puntos(contours)
-        # Show points
-        height, width = frame.shape[:2]
-        visualization = np.zeros((height, width, 3), dtype=np.uint8)
-        for x,y in mPuntos:
-            cv2.circle(visualization, (int(x), int(y)), 1, (0, 255, 0), -1)
-        cv2.imshow("Puntos 2D",visualization)
-        cv2.waitKey(0) 
+        contours=hallar_contornos(frame,binary)
+        puntos_2d = media_puntos(contours)
+        hallar_puntos_3d(puntos_2d)
 
-        hallar_puntos_3d(mPuntos)
-
-        #angulo_recorrido+=angle_per_frame
-        #if(angulo_recorrido==360-angle_per_frame):
-        #  break
+        angulo_recorrido+=angle_per_frame
+        if angulo_recorrido >= 360:
+            break
 
 
 
